bba208a0247a61987a33416fc7f0baf24fd3efb0
jangeles001 <jangeles.01d@gmail.com>
2025-04-15 15:44:02 -0700

    Completed rich-bank debugging exercise

--- rich-bank/rich-bank.js
+++ rich-bank/rich-bank.js
@@ -7,7 +7,7 @@ function getAccountById (id)
 {
 	for (const account of accounts)
 	{
-		if (account.id == id)
+		if (account.id === id)
 		{
 			return account;
 		}
@@ -16,11 +16,25 @@ function getAccountById (id)
 
 function createAccount (newAccountId, newAccountOwner)
 {
+	const account = getAccountById();
+
+	if(account){
+		throw new Error("Account already Exists.");
+	}
+
+	if(newAccountId <= 0 || !Number.isFinite(newAccountId)){
+		throw new Error("Account Id must be a positive finite number.");
+	}
+
+	if(typeof newAccountOwner !== "string" || newAccountOwner.trim() === ""){
+		throw new Error("Account name must be a string.");
+	}
+
 	accounts.push(
 		{
 			id: newAccountId,
 			owner: newAccountOwner,
-			balance: "0"
+			balance: 0
 		}
 	);
 }
@@ -31,7 +45,11 @@ function depositMoney (accountId, amount)
 
 	if (!account)
 	{
-		throw new Error("Account not found");
+		throw new Error("Account not found.");
+	}
+
+	if(amount <= 0 || !Number.isFinite(amount)){
+		throw new Error("Deposit amount must be a positive finite number.");
 	}
 
 	account.balance += amount;
@@ -46,9 +64,13 @@ function withdrawMoney (accountId, amount)
 		throw new Error("Account not found.");
 	}
 
-	if (!Number.isFinite(amount))
+	if (!Number.isFinite(amount) || amount <= 0)
 	{
 		throw new Error("Invalid value for withdrawal amount: The amount must be a finite number.");
+	}
+
+	if (amount > account.balance){
+		throw new Error("Insufficinet funds in your account to withdrawl.")
 	}
 
 	account.balance -= amount;
@@ -64,37 +86,19 @@ function transferMoney (fromAccountId, toAccountId, amount)
 		throw new Error("Source account not found.");
 	}
 
-	if (!Number.isFinite(amount) || amount < 0)
+	if (!toAccount){
+		throw new Error("Destination account not found.")
+	}
+
+	if (!Number.isFinite(amount) || amount <= 0)
 	{
 		throw new Error("Invalid value for transfer amount: The amount must be a positive finite number.");
 	}
 
+	if(amount > fromAccount.balance){
+		throw new Error("Insufficient funds in your account to transfer.")
+	}
+
+	fromAccount.balance -= amount;
 	toAccount.balance += amount;
 }
-
-/*
-Hints:
-
-getAccountById("1");
-
-createAccount(1, "Alice");
-createAccount("3", "Charlie");
-createAccount(-3, "Charlie");
-createAccount(3, ["Charlie"]);
-createAccount(3, "");
-createAccount(3, "  ");
-
-depositMoney(1, "300")
-depositMoney(1, -300)
-depositMoney(1, 0)
-depositMoney(1, Infinity)
-depositMoney(4, 100)
-
-withdrawMoney(1, -100)
-withdrawMoney(1, 0)
-withdrawMoney(1, 501)
-
-transferMoney(1, 4, 100)
-transferMoney(1, 2, 501);
-transferMoney(1, 2, 100);
-*/
