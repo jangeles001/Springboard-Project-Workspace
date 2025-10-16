672c285400b26c4bab0a52e49f157d3d982adab8
jangeles001 <207365281+jangeles001@users.noreply.github.com>
2025-10-15 21:00:07 -0700

    Added return 400 status code for mode endpoint

--- Express-Calculator/index.js
+++ Express-Calculator/index.js
@@ -5,37 +5,38 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/mean", (req, res) => {
   const nums = req.query.nums;
-  const numbers = nums.split(",").map(Number);
 
   if (!nums) {
     return res.status(400).send({ error: "nums query parameter is required!" });
   }
 
+  const numbers = nums.split(",").map(Number);
   const mean = Math.round(numbers.reduce((a, b) => a + b, 0) / nums.length);
   return res.json({ operation: "mean", mean });
 });
 
 app.get("/median", (req, res) => {
   const nums = req.query.nums;
-  const numbers = nums.split(",").map(Number);
 
   if (!nums) {
     return res.status(400).send({ error: "nums query parameter is required!" });
   }
 
+  const numbers = nums.split(",").map(Number);
   const median = Math.floor(numbers.length / 2);
   return res.json({ operation: "median", median: numbers[median] });
 });
 
 app.get("/mode", (req, res) => {
   const nums = req.query.nums;
-  const numbers = nums.split(",").map(Number);
-  const freqTable = new Map();
-  let mostFrequentNumber = numbers[0];
 
   if (!nums) {
     return res.status(400).send({ error: "nums query parameter is required!" });
   }
+
+  const numbers = nums.split(",").map(Number);
+  const freqTable = new Map();
+  let mostFrequentNumber = numbers[0];
 
   numbers.forEach((number) => {
     if (freqTable.has(number)) {
@@ -49,7 +50,7 @@ app.get("/mode", (req, res) => {
     }
   });
 
-  return res.json({ operation: "mode", mode: mostFrequentNumber });
+  return res.status(400).json({ operation: "mode", mode: mostFrequentNumber });
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
